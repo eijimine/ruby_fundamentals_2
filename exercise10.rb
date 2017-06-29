8362a090eb42acabaf83eb52bb9039fe91e69710
eiji <eijimine@gmail.com>
2017-06-29 17:10:49 -0400

    Exercise 10 part 5

---
+++ exercise10.rb
@@ -0,0 +1,23 @@
+#  1)
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+# 2)
+# def print(hash)
+#   hash.each do |key, value|
+#   puts "#{key}: #{value} students"
+#   end
+# end
+#
+# print(students)
+
+# 3)
+# students[:cohort] = 43
+# puts students
+
+# 4)
+# puts students.keys
+
+# 5)
