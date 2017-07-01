efe9054f32a69e8eae7476af467b8d60678cef19
eiji <eijimine@gmail.com>
2017-07-01 19:41:45 -0400

    Exercise 12 done

--- exercise12.rb
+++ exercise12.rb
@@ -15,34 +15,44 @@ neighbour_dogs = [
 
 # 1)
 
-# def get_absent_dogs(dogs)
-#   absent_dogs = []
-#   dogs.select do |dog|
-#     if dog[:position] >= 10
-#       absent_dogs << dog[:name]
-#    end
-#   end
-#   return absent_dogs
-# end
-#
-# absent_dogs = get_absent_dogs(my_dogs)
-# puts absent_dogs
+def get_absent_dogs(dogs)
+  absent_dogs = []
+  dogs.select do |dog|
+    if dog[:position] >= 10
+      absent_dogs << dog[:name]
+   end
+  end
+  return absent_dogs
+end
 
-# 1)
+absent_dogs = get_absent_dogs(my_dogs)
+puts absent_dogs
 
-# new_pos = my_dogs.map! do |dog|
-#   dog[:position] += 5
-# end
-#
-# puts new_pos
+# 1b)
+new_pos = my_dogs.map! do |dog|
+  dog[:position] += 5
+end
+
+puts new_pos
 
 
 # 2)
-# def squirrel(dogs)
-#   dogs.each do |dog|
-#     dog[:position] += 5
-#   end
-# end
-#
-# new_pos = squirrel(my_dogs)
-# puts new_pos
+def squirrel(dogs)
+  dogs.each do |dog|
+    dog[:position] += 5
+  end
+end
+
+new_pos = squirrel(my_dogs)
+puts new_pos
+
+# 3)
+def return_dogs(dogs)
+  dogs.map! do |dog|
+    dog[:position] = 0
+  end
+end
+
+my_dogs = return_dogs(my_dogs)
+
+puts my_dogs
